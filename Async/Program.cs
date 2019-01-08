4e66a2cba51095050dab6f43c4a1390e6ac46e9f
vitahvv <vitahvv@gmail.com>
2019-01-08 09:31:56 +0200

    1

--- Async/Program.cs
+++ Async/Program.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Async
 {
     class Program
     {
+
+        private SomeClass _someClass;        
+
+        public Program()
+        {
+            this._someClass = new SomeClass();
+        }
         static void Main(string[] args)
         {
+
+            M();
+
         }
+
+        public static void M ()
+        {
+            var a = new Program().Method("");
+            Console.WriteLine("{0}    {1}", a.Data, a.Error);
+
+        }
+
+        public SendData Method(string data)
+        {
+            return  MethodAcync(data).GetAwaiter().GetResult();
+        }
+
+        public async Task<SendData>  MethodAcync(string data)
+        {         
+            string error = null;//_someClass.QueryMethodAsync(data)
+            var result = await _someClass.QueryMethodAsync(data).ContinueWith(innerTask =>
+            {
+                if (innerTask.Status == TaskStatus.Faulted)
+                {
+                    foreach (var err in innerTask.Exception.InnerExceptions)
+                    {
+                        error += err;
+                    }
+                    return new SendData { Data = false, Error = error };
+                }
+
+                // sendData = innerTask.Result;
+                // if(innerTask.Status ==TaskStatus.RanToCompletion)
+                return innerTask.Result;
+            }, TaskContinuationOptions.ExecuteSynchronously);
+            return result;
+        }
+
     }
 }
---
+++ Async/SomeClass.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Async
+{
+    public class SomeClass
+    {
+        public SemaphoreSlim mutex = new SemaphoreSlim(1, 2);
+        string error = null;
+
+        public Task MethodAcync(string data)
+        {
+            var tsc = new TaskCompletionSource<object>();            
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    switch (data)
+                    {
+                        case "1":
+                            Console.WriteLine("{0}", data);
+                            break;
+                        case "2":
+                          //  throw null;
+                            Console.WriteLine("{0}", data);
+                            break;
+                        case "3":
+                            Console.WriteLine("{0}", data);
+                            break;
+                        default:
+                            break;
+                    }
+                    tsc.TrySetResult(null);
+                }
+                catch (Exception ex)
+                {
+                    tsc.TrySetException(ex);
+                }
+            });
+            return tsc.Task;
+        }
+
+        public async Task<SendData> QueryMethodAsync(string data)
+        {
+            SendData sendDada = null;
+            await mutex.WaitAsync().ConfigureAwait(false);
+            try
+            {
+                await this.MethodAcync(data).ContinueWith(innerTask =>
+                {
+                  //  throw null;
+                    if (innerTask.Status == TaskStatus.Faulted)
+                    {
+                        foreach (var err in innerTask.Exception.InnerExceptions)
+                        {
+                            error += err;
+                        }
+                        sendDada = new SendData { Data = false, Error = error };
+                    }
+                    if (innerTask.Status == TaskStatus.RanToCompletion)
+                        sendDada = new SendData { Data = true, Error = "good" };
+
+                }, TaskContinuationOptions.PreferFairness | TaskContinuationOptions.ExecuteSynchronously).ConfigureAwait(false);
+            }
+            finally
+            {
+                mutex.Release();
+            }
+            return sendDada;
+            //return await Task.FromResult( new SendData());
+        }
+    }
+
+    public class SendData
+    {
+        public bool Data { get; set; }
+        public string Error { get; set; }
+    }
+}
